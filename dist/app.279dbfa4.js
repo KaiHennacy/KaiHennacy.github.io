1a5ae6956c80a200366683a57a7eaae9dd708042
KaiHennacy <kchennacy@gmail.com>
2019-07-08 10:26:08 -0400

    trying to get parcel to run in deployment

--- dist/app.279dbfa4.js
+++ dist/app.279dbfa4.js
@@ -699,7 +699,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56641" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57414" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
